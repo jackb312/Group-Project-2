8a1264defddbbae734212a8a4537aed2bc36ab02
Jack Brettell <jack.brettell@gmail.com>
2019-11-09 10:46:13 -0800

    added usergames.js

---
+++ models/usergames.js
@@ -0,0 +1,40 @@
+var Sequelize = require("sequelize")
+module.exports = function(sequelize, DataTypes) {
+  var userGames = sequelize.define("usergames", {
+    id: {
+        type: DataTypes.INTEGER(100),
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    game: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    platform: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    review: {
+        type: DataTypes.INTEGER(4),
+        allowNull: false
+    },
+    game_release: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    company: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    game_character: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+  });
+  return userGames;
+};
