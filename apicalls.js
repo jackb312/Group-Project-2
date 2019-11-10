a7f605e14ee2552f6926897fc629bd43bef7b213
Ohmydearcheesus <ohmydearcheesus@gmail.com>
2019-11-10 15:40:26 -0800

    made api call export, and updated the fields for return.

--- apicalls.js
+++ apicalls.js
@@ -1,48 +1,46 @@
-require("dotenv").config();
-
-const apiKey = process.env.GIANT_BOMB;
-
 // ----------------------------------------------
 // Needs to take search query from an input field
 // ----------------------------------------------
 
+require("dotenv").config();
+const apiKey = process.env.GIANT_BOMB;
 const axios = require("axios");
+var fs = require("fs");
 
-async function makePostRequest() {
-  var query = "BoTW";
-  let res;
-  var URL;
-  try {
-    res = await axios.post(
-      (URL =
+var search = function() {
+  async function makePostRequest() {
+    var query = "breath+of+the+wild";
+    var plats = [];
+    let res;
+    try {
+      res = await axios.post(
         "http://www.giantbomb.com/api/search/?api_key=" +
-        apiKey +
-        "&format=json&query=" +
-        query)
-    );
-    console.log(`Release day: ${res.results[0].expected_release_day}`);
-  } catch (err) {
-    console.log("error in fetching posts");
-    console.log(URL);
+          apiKey +
+          "&format=json&query=" +
+          query
+      );
+      for (var i = 0; i < res.data.results[0].platforms.length; i++) {
+        plats.push(res.data.results[0].platforms[i]);
+      }
+      return {
+        gameTitle: res.data.results[0].image_tags.name,
+        rating: res.data.results[0].original_game_rating[0].name,
+        releaseDate:
+          res.data.results[0].expected_release_month +
+          "/" +
+          res.data.results[0].expected_release_day +
+          "/" +
+          res.data.results[0].expected_release_year,
+        platforms: plats,
+        image: res.data.results[0].image.original_url
+      };
+
+      // console.log(`Release day: ${res.results[0].expected_release_day}`);
+    } catch (err) {
+      console.log("error in fetching posts");
+      console.log(err);
+    }
   }
-}
-// console.log(`Status text: ${res.statusText}`);
-// console.log(`Request method: ${res.request.method}`);
-// console.log(`Path: ${res.request.path}`);
+};
 
-// console.log(`Date: ${res.headers.date}`);
-// console.log(`Data: ${res.data}`);
-
-makePostRequest();
-
-//
-// $ node post_request.js
-//
-// Status code: 201
-// Status text: Created
-// Request method: POST
-// Path: /posts
-// Date: Mon, 04 Feb 2019 16:54:36 GMT
-// Data: [object Object]
-//
-// This is the output.
+module.exports = search;
